cf3b33e0622c9acb013b04828fa56581acb2ae05
p4614655 <p4614655@gmail.com>
2025-05-04 20:44:10 +0100

    Update extractProduct.js

--- scraper/extractProduct.js
+++ scraper/extractProduct.js
@@ -1,10 +1,12 @@
+// scraper/extractProduct.js
 const scrapeGuitarSalon = require('../shops/scrapeGuitarSalon.selenium');
 
 async function extractProductInfo(url) {
   if (url.includes('guitarsalon.com')) {
     return await scrapeGuitarSalon(url);
+  } else {
+    throw new Error('Website not supported.');
   }
-  throw new Error('Unsupported shop URL.');
 }
 
 module.exports = { extractProductInfo };
