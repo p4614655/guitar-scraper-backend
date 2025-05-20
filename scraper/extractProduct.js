9db126edfa0e452e8b3df472e7da49dfc1d6e746
p4614655 <p4614655@gmail.com>
2025-05-20 20:06:30 +0100

    Update extractProduct.js

--- scraper/extractProduct.js
+++ scraper/extractProduct.js
@@ -3,11 +3,11 @@ const scrapeGuitarSalonSelenium = require('../shops/scrapeGuitarSalon.selenium')
 
 async function extractProductInfo(url, method = 'selenium') {
   const hostname = new URL(url).hostname;
+
   if (hostname.includes('guitarsalon.com')) {
-    if (method === 'selenium') {
-      return await scrapeGuitarSalonSelenium(url);
-    }
+    return await scrapeGuitarSalonSelenium(url);
   }
+
   throw new Error('Unsupported website or method.');
 }
 
